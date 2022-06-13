75702ccdffa1468de07baf4a5055393b9c449e7c
gatwi <faith.murithi@student.moringaschool.com>
2022-06-13 16:32:02 +0300

    commit to master

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+// to prevent submit from doing its default behavior in a similar fashion.
+
+const createNewTask = Event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("list");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+}
+
+// Additional input fields
+const newTaskForm = document.getElementById("create-task-form");
+const newTaskDescription = document.getElementById("new-task-description");
